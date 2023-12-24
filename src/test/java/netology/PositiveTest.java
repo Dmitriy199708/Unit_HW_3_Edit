0b70456e99494cd22d3d6d26e963edddb1bb32c3
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-12-24 18:41:11 +0300

    eit projects

--- src/test/java/netology/PositiveTest.java
+++ src/test/java/netology/PositiveTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
